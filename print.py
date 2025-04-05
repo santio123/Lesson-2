904cfdda228af93fa3e822474d73581a02157968
santio123 <santioflorin1007@gmail.com>
2025-04-05 09:33:03 +0530

    hi

---
+++ print.py
@@ -0,0 +1,16 @@
+#Print a sentence
+print("Welcome to the world of programming!")
+
+#Print a number
+print(4)
+
+#Next Line after print
+print("Hello python \n")
+
+#Pass multiple arguments for printing
+print("hello",5)
+
+#End argument of print Statement
+print("welcome to", end= " ")
+
+print("my house")
